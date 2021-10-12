e7f211d6e46bd27de13490ebd4d5545cf1cac65f
SHREESH PANDEY <shvspandey@gmail.com>
2021-10-12 20:49:52 +0530

    Employee is Present or Absent

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ namespace Employeewage
     {
         static void Main(string[] args)
         {
-            //Start Message to Display Welcome Message as the Output in the master branch
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            int FullTimeEmployee = 1;
+            Random random = new Random();
+            int Check = random.Next(0, 2);//Random Number Generated
+            if (Check == FullTimeEmployee)//if check=1
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else//if check=0
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
