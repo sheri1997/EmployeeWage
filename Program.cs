7e23e644ff902fde0f0027fe5d21c3150d2d5efb
SHREESH PANDEY <shvspandey@gmail.com>
2021-10-12 20:41:59 +0530

    Displaying welcome message as the output

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employeewage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start Message to Display Welcome Message as the Output in the master branch
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
