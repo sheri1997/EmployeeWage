5dc88f9b0bc4f05b9e8a3428b85e9f36d119a456
SHREESH PANDEY <shvspandey@gmail.com>
2021-10-14 18:47:16 +0530

    Calculation of Employee Wage for Multiple Companies

--- Program.cs
+++ Program.cs
@@ -4,29 +4,39 @@ namespace Employeewage
 {
     class Program
     {
+        public const int PartTime = 1;
+        public const int FullTime = 2;
+        public static int empwage(string company,int emprateperhour,int noofwokingdays,int maxhourspermonth)
+        {
+            int emphours = 0, totalemphours = 0, totalworkingdays = 0;
+            while(totalemphours<=maxhourspermonth && totalworkingdays<noofwokingdays)
+            {
+                totalworkingdays++;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch(empcheck)
+                {
+                    case PartTime:
+                        emphours = 8;
+                        break;
+                    case FullTime:
+                        emphours = 16;
+                        break;
+                    default:
+                        emphours = 0;
+                        break;
+                }
+                totalemphours += emphours;
+                Console.WriteLine("Day#" + totalworkingdays + "Emp Hrs" + emphours);
+            }
+            int totalempwage = totalemphours * emprateperhour;
+            Console.WriteLine("Total employee Wage for the company" + company + totalempwage);
+            return totalempwage;
+        }
         static void Main(string[] args)
         {
-            const int PartTime = 1;
-            const int FullTimeEmployee = 2;
-            int WagePerHour = 20;
-            int Hours = 0;
-            int Wage = 0;
-            Random random = new Random();
-            int Check = random.Next(0, 3);
-            switch (Check)
-            {
-                case PartTime:
-                    Hours = 8;
-                    break;
-                case FullTimeEmployee:
-                    Hours = 16;
-                    break;
-                default:
-                    Hours = 0;
-                    break;
-            }
-            Wage = Hours * WagePerHour;
-            Console.WriteLine("Wage of The Part Time Employee is " + Wage);
+            empwage("Dmart", 20, 2, 10);
+            empwage("Reliance", 10, 4, 20);
         }
     }
 }
